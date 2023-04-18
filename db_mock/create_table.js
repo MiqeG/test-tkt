6ead3e3faa1e1b1c9895978558d18df58ab3a3f4
MiqeG <mghilescu@protonmail.com>
2023-04-18 09:54:29 +0200

    updated create_table to have success message

--- db_mock/create_table.js
+++ db_mock/create_table.js
@@ -2,7 +2,7 @@ const db = require("../app/dist/db/db");
 creatTable();
 async function creatTable() {
   try {
-    console.log(await db.create());
+    console.log(await db.create(), "DataBase Created !");
   } catch (error) {
     console.error("DB CREATION ERROR ", error);
   }
